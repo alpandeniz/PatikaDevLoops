5ed8a2a8960cec21892bf83500b92512f97d4d0c
Alpan <alpandnz48@gmail.com>
2022-11-25 20:33:34 +0300

    www.patika.dev - Homework - Java101

---
+++ OddNumberFinder/src/OddNumberFinder.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class OddNumberFinder {
+
+	public static void main(String[] args) {
+		
+		int total2=0, total = 0;
+		
+		Scanner scanner = new Scanner(System.in);
+		boolean isOddNumber = false;
+		while(!isOddNumber) {
+			
+			System.out.print("Lütfen sayı giriniz: ");
+			int n = scanner.nextInt();
+			if(!(n%2 == 0)) {
+				isOddNumber = true;
+			}else if(n%4==0) {
+				total += n;
+			}
+			
+		}
+		System.out.println("4'e bölünen sayıların toplamı = " + total);
+		
+		}
+
+}
